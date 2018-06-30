ee46a533028093ea519ce3c8c5fde380016d3898
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-06-30 20:03:41 +0200

    remove unnecesary code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,2 @@
 export { BTT } from './btt';
-import { BTT } from './btt';
 export * from '../types';
-
-// initialize the BTT providing the data where the server is hosted
-const btt = new BTT({
-    domain: '127.0.0.1',
-    port: 64472,
-    protocol: 'http',
-    sharedKey: 'sBP2fYAo2Fu8TdfzhLpwdUm'
-  });
-  
-  // btt.triggerShortcut('cmd+space')
-    // .then(() => btt.showHUD({ title: ':D :D 👌😹'}));
-  
-  
-    btt.showHUD({ title: ':D :D 👌😹'});
-  
